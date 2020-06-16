8cf0946dad0d8a2d9f76b570f75b7eada7f65c8d
st_nekto <deform@tut.by>
2020-06-16 11:26:15 +0300

    create entities "Company" and "Employee"

---
+++ src/main/java/com/nevermind/springboot/lesson/entity/EmployeeEntity.java
@@ -0,0 +1,34 @@
+package com.nevermind.springboot.lesson.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "employee")
+public class EmployeeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private LocalDate birthDay;
+
+    private Integer salary;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private CompanyEntity company;
+}
