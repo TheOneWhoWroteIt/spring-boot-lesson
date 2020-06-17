8ef85ccb1b02ea946b1997527aa07b65e22bdab2
st_nekto <deform@tut.by>
2020-06-17 11:59:47 +0300

    create test for "Company"

---
+++ src/test/java/com/nevermind/springboot/lesson/repository/CompanyRepositoryTest.java
@@ -0,0 +1,55 @@
+package com.nevermind.springboot.lesson.repository;
+
+import com.nevermind.springboot.lesson.entity.CompanyEntity;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@Transactional
+class CompanyRepositoryTest {
+
+    private static final Integer GOOGLE_ID = 1;
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+
+
+    @Test
+    void testGetById(){
+        Optional<CompanyEntity> company = companyRepository.findById(GOOGLE_ID);
+        assertTrue(company.isPresent());
+        company.ifPresent(companyEntity -> {
+            assertEquals("Google", companyEntity.getName());
+        });
+
+    }
+
+    @Test
+    void testSave(){
+        CompanyEntity intel = CompanyEntity.builder()
+                .name("Intel")
+                .build();
+        companyRepository.save(intel);
+        assertNotNull(intel.getId());
+    }
+
+}
