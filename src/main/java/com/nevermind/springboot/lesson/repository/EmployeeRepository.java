0cac7377f9765b6299304a4d6132fd4ee6a2ebab
st_nekto <deform@tut.by>
2020-06-17 14:46:09 +0300

    create custom queries and test

--- src/main/java/com/nevermind/springboot/lesson/repository/EmployeeRepository.java
+++ src/main/java/com/nevermind/springboot/lesson/repository/EmployeeRepository.java
@@ -4,6 +4,13 @@ import com.nevermind.springboot.lesson.entity.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Integer> {
+
+    Optional<EmployeeEntity> findByFirstNameContaining(String firstName);
+
+    List<EmployeeEntity> findByFirstNameAndSalary(String firstName, Integer salary);
 }
--- src/test/java/com/nevermind/springboot/lesson/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/nevermind/springboot/lesson/repository/EmployeeRepositoryTest.java
@@ -2,9 +2,12 @@ package com.nevermind.springboot.lesson.repository;
 
 import com.nevermind.springboot.lesson.IntegrationTestBase;
 import com.nevermind.springboot.lesson.entity.EmployeeEntity;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.collection.IsCollectionWithSize;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,12 +18,23 @@ class EmployeeRepositoryTest extends IntegrationTestBase {
 
     @Autowired
     private EmployeeRepository employeeRepository;
-    
+
 
     @Test
     void testFindById(){
         Optional<EmployeeEntity> tim = employeeRepository.findById(TIM_ID);
         assertTrue(tim.isPresent());
+    }
+
+    @Test
+    void testFindByFirstName(){
+        Optional<EmployeeEntity> tim = employeeRepository.findByFirstNameContaining("Tim");
+        assertTrue(tim.isPresent());
+    }
+
+    void testFindByFirstNameAndSalary(){
+        List<EmployeeEntity> tim = employeeRepository.findByFirstNameAndSalary("Tim", 1000);
+        MatcherAssert.assertThat(tim, IsCollectionWithSize.hasSize(1));
     }
 
 }
