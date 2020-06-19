7e48b0705611da1bf0112664d4ae8faca6cef3b2
st_nekto <deform@tut.by>
2020-06-19 09:13:05 +0300

    create interface for native projection query

---
+++ src/main/java/com/nevermind/springboot/lesson/projection/EmployeeNativeView.java
@@ -0,0 +1,8 @@
+package com.nevermind.springboot.lesson.projection;
+
+public interface EmployeeNativeView {
+
+    Integer getId();
+
+    String getFullName();
+}
--- src/main/java/com/nevermind/springboot/lesson/repository/EmployeeRepository.java
+++ src/main/java/com/nevermind/springboot/lesson/repository/EmployeeRepository.java
@@ -2,7 +2,10 @@ package com.nevermind.springboot.lesson.repository;
 
 import com.nevermind.springboot.lesson.entity.EmployeeEntity;
 import com.nevermind.springboot.lesson.projection.EmployeeNameView;
+import com.nevermind.springboot.lesson.projection.EmployeeNativeView;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,4 +19,11 @@ public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Intege
     List<EmployeeEntity> findByFirstNameAndSalary(String firstName, Integer salary);
 
     List<EmployeeNameView> findAllBySalaryGreaterThan(Integer salary);
+
+    @Query(value = "select " +
+            "e.id as id, " +
+            "e.first_name || e.last_name as fillName " +
+            "from employee e " +
+            "where e.salary > :salary", nativeQuery = true)
+    List<EmployeeNativeView> findAllBySalaryGreaterThanNative(@Param("salary") Integer salary);
 }
