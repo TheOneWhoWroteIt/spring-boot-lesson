31b98ed135ab86f88c69a7199c6efd5ec5cc2bdb
st_nekto <deform@tut.by>
2020-07-01 14:37:30 +0300

    create initializer for test container

--- src/test/java/com/nevermind/springboot/lesson/IntegrationTestBase.java
+++ src/test/java/com/nevermind/springboot/lesson/IntegrationTestBase.java
@@ -1,11 +1,22 @@
 package com.nevermind.springboot.lesson;
 
+import com.nevermind.springboot.lesson.initializer.Postgres;
+import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 @ActiveProfiles("test")
 @SpringBootTest
+@ContextConfiguration(initializers = {
+        Postgres.Initializer.class
+})
 @Transactional
 public abstract class IntegrationTestBase {
+
+    @BeforeAll
+    static void init(){
+        Postgres.container.start();
+    }
 }
